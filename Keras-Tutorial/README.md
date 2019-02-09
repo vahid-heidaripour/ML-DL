4883bd1c8b376a77e46da12acf952a346b203fb0
Vahid <v.heidaripour@gmail.com>
2019-02-09 21:56:02 +0100

    keras files

---
+++ Keras-Tutorial/README.md
@@ -0,0 +1,3 @@
+# Keras Tutorial
+Keras, a high-level neural networks API (programming framework), written in Python and capable of running on top of several lower-level frameworks including TensorFlow and CNTK.
+
---
+++ Keras-Tutorial/code-v1.py
@@ -0,0 +1,74 @@
+import numpy as np
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from kt_utils import *
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+
+# %matplotlib inline
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Reshape
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+
+# GRADED FUNCTION: HappyModel
+
+def HappyModel(input_shape):
+    """
+    Implementation of the HappyModel.
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+
+    X_input = Input(input_shape)
+
+    # Zero-Padding: pads the border of X_input with zeroes
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # CONV -> BN -> RELU Block applied to X
+    X = Conv2D(32, (7, 7), strides=(1, 1), name='conv0')(X)
+    X = BatchNormalization(axis=3, name='bn0')(X)
+    X = Activation('relu')(X)
+
+    # MAXPOOL
+    X = MaxPooling2D((2, 2), name='max_pool')(X)
+
+    # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
+    X = Flatten()(X)
+    X = Dense(1, activation='sigmoid', name='fc')(X)
+
+    model = Model(inputs=X_input, outputs=X, name='HappyModel')
+
+    return model
+
+happyModel = HappyModel(X_train.shape[1:])
+
+happyModel.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+
+happyModel.fit(X_train, Y_train, epochs=40, batch_size=50)
+
+happyModel.summary()
