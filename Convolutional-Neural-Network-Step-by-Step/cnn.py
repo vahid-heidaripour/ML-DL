15e7d5b422cb9107aebb8dd73448f21b09486678
Vahid <v.heidaripour@gmail.com>
2019-02-28 15:17:56 +0100

    mask

--- Convolutional-Neural-Network-Step-by-Step/cnn.py
+++ Convolutional-Neural-Network-Step-by-Step/cnn.py
@@ -180,6 +180,20 @@ def conv_backward(dZ, cache):
     return dA_prev, dW, db
 
 
+def create_mask_from_window(x):
+    """
+    Creates a mask from an input matrix x, to identify the max entry of x.
+
+    Arguments:
+    x -- Array of shape (f, f)
+
+    Returns:
+    mask -- Array of the same shape as window, contains a True at the position corresponding to the max entry of x.
+    """
+
+    mask = x == np.max(x)
+
+    return mask
 
 
 
