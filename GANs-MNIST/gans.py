0e1332688ebdb0e30734987989527017428b56ac
Vahid <v.heidaripour@gmail.com>
2019-03-16 20:03:21 +0100

    generator

--- GANs-MNIST/gans.py
+++ GANs-MNIST/gans.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 
 mist = input_data.read_data_sets("MNIST_data")
+
+def generator(Z, hsize=[128, 128], reuse = False):
+    with tf.variable_scope("GAN/Generator", reuse=reuse):
+        h1 = tf.layers.dense(Z, hsize[0], activation=tf.nn.relu)
+        h2 = tf.layers.dense(h1, hsize[1], activation=tf.nn.relu)
+        out = tf.layers.dense(h2, 784, activation=tf.nn.tanh)
+
+        return out
