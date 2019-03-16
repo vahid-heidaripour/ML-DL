e254e336b7af9b1e9b33784dc5031cf42b1cfe52
Vahid <v.heidaripour@gmail.com>
2019-03-16 19:52:28 +0100

    mnist

---
+++ GANs-MNIST/gans.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+
+mist = input_data.read_data_sets("MNIST_data")
